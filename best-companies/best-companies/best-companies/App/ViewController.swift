5d8984697927b0742ebbaad4660a23f42c0bb182
Лев Бондаренко <>
2019-06-11 08:54:10 +0500

    add api response

--- best-companies/best-companies/best-companies/App/ViewController.swift
+++ best-companies/best-companies/best-companies/App/ViewController.swift
@@ -12,12 +12,27 @@ class HomeViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    view.backgroundColor = .white
     guard let url = URL(string: "http://megakohz.bget.ru/test.php") else { return }
     
     let apiClient = APIClient()
     
+    print("test")
     apiClient.get(path: url) { result in
-      print(result)
+      print("test")
+      switch result {
+      case .success(let data):
+        do {
+          let response = try JSONDecoder().decode(SingleResponse<Company>.self, from: data)
+          
+          print(response)
+        } catch {
+          print(error)
+        }
+      case .failure(let error):
+        print("error", error)
+      }
+      
     }
     
     print(apiClient)
--- best-companies/best-companies/best-companies/Domains/Response.swift
+++ best-companies/best-companies/best-companies/Domains/Response.swift
@@ -18,3 +18,25 @@ public struct Response<T: Codable> {
     self.data = data
   }
 }
+
+public struct SingleResponse<T: Codable> {
+  public let status: Int
+  public let success: Bool
+  public let data: T
+  
+  public init(status: Int,
+              success: Bool,
+              data: T) {
+    self.status = status
+    self.success = success
+    self.data = data
+  }
+}
+
+extension SingleResponse: Codable {
+  enum ResponseKeys: String, CodingKey {
+    case status
+    case success
+    case data
+  }
+}
